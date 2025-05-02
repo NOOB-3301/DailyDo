47716cc53a6fe2ffe0f97d4ccf67174b5443622c
NOOB-3301 <sepic466@gmail.com>
2025-05-02 12:06:17 +0530

    Remove commented-out code from RootLayout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,23 +2,6 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    // <Stack
-    //   screenOptions={{
-    //   headerStyle: {
-    //     backgroundColor: '#f4511e',
-    //   },
-    //   headerTintColor: '#fff',
-    //   headerTitleStyle: {
-    //     fontWeight: 'bold',
-    //   },
-    //   }}>
-    //   <Stack.Screen name="(tabs)" options={{ title: "ToDo App" }} />
-    //   <Stack.Screen 
-    //   name="tasks/[task]" 
-    //   options={{
-    //     title: "Task Details",}} 
-    //   />
-    // </Stack>
     <Stack>
       <Stack.Screen
         name="index"
